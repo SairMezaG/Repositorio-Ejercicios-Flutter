e9c2f4a80b251c562931d7bd327287e05456597e
kathe <Vasquezdianakatherine@gmail.com>
2024-06-06 12:27:10 -0500

    Se efectuan las actualizaciones

---
+++ lib/ejercicio1/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:aplicacion_login/ejercicio1/homePrincipal.dart';
+
+void main() {
+  runApp(const Principal());
+}
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: "Aplicación Hospital",
+        debugShowCheckedModeBanner: false,
+        home: HomePrincipal());
+  }
+}
