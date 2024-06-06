f766a949770ab9b15a9ebbebb6a7ccd031f25a5f
SairMezaG <z.meza.g@hotmail.com>
2024-06-06 14:37:02 -0500

    Se realiza el tercer proyecto

--- lib/ejercicio3/deslizarPagina.dart
+++ lib/ejercicio3/deslizarPagina.dart
@@ -7,17 +7,17 @@ class DeslizarPagina extends StatelessWidget {
     return PageView(
       children: <Widget>[
         DeslizarItem(
-          imagePath: 'assets/https://www.pngwing.com/en/search?q=matrix',
+          imagePath: 'https://tse3.mm.bing.net/th?id=OIP.FiUOpmri_2pS3zjeNi8KnAHaG8&pid=Api&P=0&h=180/600x400',
           title: 'Aprendiz',
           subtitle: 'Inscríbete al Sena',
         ),
-       DeslizarItem(
-          imagePath: 'assets/image2.png',
+        DeslizarItem(
+          imagePath: 'https://tse3.mm.bing.net/th?id=OIP.FiUOpmri_2pS3zjeNi8KnAHaG8&pid=Api&P=0&h=180/600x400',
           title: 'Prepárate',
           subtitle: 'El futuro es hoy',
         ),
         DeslizarItem(
-          imagePath: 'assets/image3.png',
+          imagePath: 'https://tse3.mm.bing.net/th?id=OIP.FiUOpmri_2pS3zjeNi8KnAHaG8&pid=Api&P=0&h=180/600x400',
           title: 'Programación de software',
           subtitle: 'Despierta tu mente',
         ),
@@ -25,3 +25,4 @@ class DeslizarPagina extends StatelessWidget {
     );
   }
 }
+
